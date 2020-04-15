10e12b3efb55a45b46742cfec5e14d60ad33f9ae
fluffyhake <haakon.reppen@gmail.com>
2020-04-15 11:42:17 +0200

    Added dictionary input functionality

--- draft/youtube_dl_simple.py
+++ draft/youtube_dl_simple.py
@@ -1,15 +1,18 @@
 #This program contains the 
 import os
 import youtube_dl
+import pprint
 
 
 
-
-ydl_opts = {
-
+def download(url, **add_args):
+    ydl_opts = {
 }
-
-def download(url):
+    for key,value in add_args.items():
+        ydl_opts[key] = value
+        print(ydl_opts)
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
+
+download(url = "https://www.youtube.com/watch?v=0Kvw2BPKjz0", format = "mp4")
